a599e0119a4a1005a54ef5c992e9035747d2c27b
luffypzheng <736699012@qq.com>
2020-06-17 16:07:22 +0800

    test2

--- app/src/main/java/com/example/shufflingfigure/MainActivity.java
+++ app/src/main/java/com/example/shufflingfigure/MainActivity.java
@@ -120,10 +120,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
+
 
     private void initPoints() {
         for (int i = 0; i < mShufflingAdapter.getDataRealSize(); i++) {
